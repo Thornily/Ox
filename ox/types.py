b7f494ba43e0ec96a595dc5a0a9bcae0048275a8
Thornily <119772338+Thornily@users.noreply.github.com>
2022-12-08 17:29:53 +0000

    Added Files

---
+++ ox/types.py
@@ -0,0 +1,13 @@
+import typing
+
+class ArrayType(type): 
+    """
+    The type class for Arrays in OX
+    """
+    pass
+
+class MatrixType(type): 
+    """
+    The type class for Matrices in OX
+    """
+    pass
